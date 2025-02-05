4b4318e8227e05a5229de5ef72c2f0b039938221
MaiLieRim <liedlmiriam@gmail.com>
2025-02-05 10:59:18 +0100

    update passwordInput

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,4 +1,14 @@
-
+<style lang="css" scoped>
+.btn-outline-secondary{ 
+    border: var(--bs-border-width) solid var(--bs-border-color);
+    border-left: 0cap;
+    color:--bs-border-color;
+    --bs-btn-hover-bg:white;
+}
+#inputPassword{
+    border-right: 0cap;
+}
+</style>
 <template>
     <div class="container p-3">
         <img src="/src/assets/images/logo.png " class="w-50">
@@ -7,12 +17,12 @@
         <form class="mt-3 text-primary" @submit.prevent="login">
             <div>
                 <label for="username" class=" form-label">Benutzer</label>
-                <input type="text" id="inputPassword5" class="form-control" aria-describedby="passwordHelpBlock"  v-model="email">
+                <input type="text" id="inputemail" class="form-control" aria-describedby="passwordHelpBlock"  v-model="email">
             </div>
             <div>
-                <label for="inputPassword5" class=" form-label">Passwort</label>
+                <label for="inputPassword" class=" form-label">Passwort</label>
                 <div class="input-group mb-3">
-                    <input type="password" id="inputPassword5" class="form-control"
+                    <input type="password" id="inputPassword" class="form-control"
                         aria-describedby="passwordHelpBlock"  v-model="password">
                     <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="bi bi-eye-slash"></i> </button>
                 </div>               
@@ -25,7 +35,7 @@
         </div>
         <div class="d-flex gap-2 justify-content-end ">
             <router-link to="/registry" class="btn btn-outline-primary" type="submit">Registrieren</router-link>
-            <router-link to="/" class="btn btn-primary text-light" @click="login()" type="submit">Anmelden</router-link>
+            <router-link to="/" class="btn btn-primary text-light" :onClick="login" type="submit">Anmelden</router-link>
         </div>
     </div>
     <Fingerprint></Fingerprint>
@@ -39,7 +49,7 @@ import Fingerprint from '@/components/Fingerprint.vue';
 const email = ref('');
 const password = ref('');
 
-function login() {
+const login = () => {
     localStorage.setItem('authToken', 'your-auth-token');
 }
 </script>
